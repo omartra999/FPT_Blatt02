a0f346d0514590be1f440a0d968332565500a785
Omar <omartra069@gmail.com>
2024-08-14 18:37:25 +0200

    addChapter method

--- src/Buch.java
+++ src/Buch.java
@@ -1,5 +1,6 @@
 public class Buch {
    private Kapitel ersteKapitel;
+   private Kapitel[] kapitels;
    private Autor autor;
 
    public Buch(Kapitel ersteKapitel, Autor autor){
@@ -21,5 +22,30 @@ public class Buch {
 
     public void setAutor(Autor autor) {
         this.autor = autor;
+    }
+
+    public Kapitel[] getKapitels() {
+        return kapitels;
+    }
+
+    public void setKapitels(Kapitel[] kapitels) {
+        this.kapitels = kapitels;
+    }
+
+    public void addChapter(String title, Text text){
+       //define a new kapitel from the information we have from parameter
+        Kapitel newKapitel = new Kapitel(title, text, null);
+        //define a new kapitels list with the one more entery size than the original
+        Kapitel[] newKapitels = new Kapitel[this.kapitels.length + 1];
+
+        //copy the old kapitels in the new array
+        for (int i = 0; i < this.kapitels.length ; i++) {
+            newKapitels[i] = this.kapitels[i];
+        }
+        //add our new chapter in the last index of the new array
+        newKapitels[newKapitels.length] = newKapitel;
+
+        //now update our orginal array to be the same as the new one
+        this.kapitels = newKapitels;
     }
 }
