dfd6974575bb1561079272435d6a9a724ce18d72
Omar <omartra069@gmail.com>
2024-08-14 18:11:22 +0200

    trying to resolve issues

--- src/Buch.java
+++ src/Buch.java
@@ -1,2 +1,15 @@
 public class Buch {
+   private Kapitel ersteKapitel;
+
+   public Buch(Kapitel ersteKapitel){
+       this.ersteKapitel = ersteKapitel;
+   }
+
+    public Kapitel getErsteKapitel() {
+        return ersteKapitel;
+    }
+
+    public void setErsteKapitel(Kapitel ersteKapitel) {
+        this.ersteKapitel = ersteKapitel;
+    }
 }
